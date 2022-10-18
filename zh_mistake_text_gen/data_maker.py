563418b263d6db59a01121fa32424d6e2a889ba6
Philip <p208p2002@gmail.com>
2022-10-18 00:48:02 +0000

    style: pylint

--- zh_mistake_text_gen/data_maker.py
+++ zh_mistake_text_gen/data_maker.py
@@ -1,12 +1,12 @@
+from .utils import Pronounce2Word
+from .data_model import NoiseCorpus
+from .exception import DataNotFundError, FindOrConvertError, DataGenerationError
 import random
 from loguru import logger
 import jieba
-from .utils import Pronounce2Word
 from abc import ABC
 from opencc import OpenCC
 from typing import Any
-from .data_model import NoiseCorpus
-from .exception import DataNotFundError,FindOrConvertError, DataGenerationError
 import os
 import py_chinese_pronounce
 from copy import copy
@@ -16,8 +16,9 @@ high_freq_zh_char_path = os.path.join(
     'high_freq_zh_char.txt'
 )
 
+
 class BaseDataMaker(ABC):
-    def __init__(self,*args,**kwargs) -> None:
+    def __init__(self, *args, **kwargs) -> None:
         self.t2s = OpenCC('t2s.json').convert
         self.setup()
 
@@ -27,11 +28,11 @@ class BaseDataMaker(ABC):
         """
         pass
 
-    def make(self,x)->NoiseCorpus:
+    def make(self, x) -> NoiseCorpus:
         raise NotImplementedError
-    
-    def __call__(self, *args: Any, **kwargs: Any)-> NoiseCorpus:
-        data = self.make(*args,**kwargs)
+
+    def __call__(self, *args: Any, **kwargs: Any) -> NoiseCorpus:
+        data = self.make(*args, **kwargs)
         data.type = self.__class__.__name__
 
         if self.t2s(data.correct) == self.t2s(data.incorrect):
@@ -39,15 +40,17 @@ class BaseDataMaker(ABC):
 
         return data
 
+
 class NoChangeMaker(BaseDataMaker):
-    def make(self, x)->NoiseCorpus:
+    def make(self, x) -> NoiseCorpus:
         return NoiseCorpus(
             correct=x,
             incorrect=x,
         )
 
+
 class MissingWordMaker(BaseDataMaker):
-    def make(self, x)->NoiseCorpus:
+    def make(self, x) -> NoiseCorpus:
         correct = x[:]
         rand = random.randint(0, len(x)-1)
         x = list(x)
@@ -59,8 +62,9 @@ class MissingWordMaker(BaseDataMaker):
             incorrect=x,
         )
 
+
 class MissingVocabMaker(BaseDataMaker):
-    def make(self,x)->NoiseCorpus:
+    def make(self, x) -> NoiseCorpus:
         correct = x[:]
         seg_list = list(jieba.cut(x))
         rand = random.randint(0, len(seg_list)-1)
@@ -71,18 +75,20 @@ class MissingVocabMaker(BaseDataMaker):
             incorrect=''.join(seg_list)
         )
 
+
 class PronounceSimilarWordMaker(BaseDataMaker):
     """
     去除聲符找相似
     """
-    def __init__(self,*args,p2w=None,**kwargs):
+
+    def __init__(self, *args, p2w=None, **kwargs):
         super().__init__()
         if p2w is not None:
             self.p2w = p2w
         else:
             self.p2w = Pronounce2Word()
 
-    def make(self,x):
+    def make(self, x):
         correct = x[:]
         rand = random.randint(0, len(x)-1)
 
@@ -90,8 +96,8 @@ class PronounceSimilarWordMaker(BaseDataMaker):
 
         try:
             similar_vocab = self.p2w.find_similar(replace_word)
-        except:
-            raise FindOrConvertError('p2w find similar error')
+        except Exception as exc:
+            raise FindOrConvertError('p2w find similar error') from exc
 
         rand_for_select_similar_word = random.randint(0, len(similar_vocab)-1)
         select_similar_word = similar_vocab[rand_for_select_similar_word]
@@ -105,18 +111,20 @@ class PronounceSimilarWordMaker(BaseDataMaker):
             incorrect=x
         )
 
+
 class PronounceSimilarWordPlusMaker(BaseDataMaker):
     """
     編輯距離找相似+高頻字
     """
-    def __init__(self,*args,p2w=None,level=1,**kwargs):
+
+    def __init__(self, *args, p2w=None, level=1, **kwargs):
         super().__init__()
         self.level = level
         if p2w is not None:
             self.p2w = p2w
         else:
             self.p2w = Pronounce2Word()
-    
+
     def setup(self):
         self.high_freq_zh_char = []
         f = open(high_freq_zh_char_path, encoding='utf-8')
@@ -124,22 +132,23 @@ class PronounceSimilarWordPlusMaker(BaseDataMaker):
             self.high_freq_zh_char.append(line.replace('\n', ''))
         f.close()
 
-
-    def make(self,x):
+    def make(self, x):
         correct = x[:]
         rand = random.randint(0, len(x)-1)
 
         replace_word = x[rand]
 
         try:
-            new_han_pronounces = self.p2w._find_similar_han_pronounces(self.p2w.to_han(replace_word),level=self.level)
-        except:
-            raise FindOrConvertError
+            new_han_pronounces = self.p2w._find_similar_han_pronounces(
+                self.p2w.to_han(replace_word), level=self.level)
+        except Exception as exc:
+            raise FindOrConvertError from exc
 
         random.shuffle(new_han_pronounces)
         new_han_pronounce = new_han_pronounces[0]
         new_words = self.p2w.han2word(new_han_pronounce)
-        new_words = list(filter(lambda x:x in self.high_freq_zh_char,new_words))
+        new_words = list(
+            filter(lambda x: x in self.high_freq_zh_char, new_words))
 
         if len(new_words) == 0:
             raise DataNotFundError("No high freq char in string")
@@ -149,7 +158,7 @@ class PronounceSimilarWordPlusMaker(BaseDataMaker):
 
         if new_word == replace_word:
             raise DataNotFundError("same word")
-        
+
         rand_for_select_similar_word = random.randint(0, len(new_word)-1)
         select_similar_word = new_word[rand_for_select_similar_word]
 
@@ -162,15 +171,16 @@ class PronounceSimilarWordPlusMaker(BaseDataMaker):
             incorrect=x
         )
 
+
 class PronounceSameWordMaker(BaseDataMaker):
-    def __init__(self,*args,p2w=None,**kwargs):
+    def __init__(self, *args, p2w=None, **kwargs):
         super().__init__()
         if p2w is not None:
             self.p2w = p2w
         else:
             self.p2w = Pronounce2Word()
-            
-    def make(self,x):
+
+    def make(self, x):
         correct = x[:]
         rand = random.randint(0, len(x)-1)
 
@@ -178,8 +188,8 @@ class PronounceSameWordMaker(BaseDataMaker):
 
         try:
             similar_vocab = self.p2w.find_same(replace_word)
-        except:
-            raise FindOrConvertError
+        except Exception as exc:
+            raise FindOrConvertError from exc
 
         if len(similar_vocab) == 0:
             raise DataNotFundError('similar_vocab not found')
@@ -198,14 +208,14 @@ class PronounceSameWordMaker(BaseDataMaker):
 
 
 class PronounceSimilarVocabMaker(BaseDataMaker):
-    def __init__(self,*args,p2w=None,**kwargs):
+    def __init__(self, *args, p2w=None, **kwargs):
         super().__init__()
         if p2w is not None:
             self.p2w = p2w
         else:
             self.p2w = Pronounce2Word()
 
-    def make(self,x):
+    def make(self, x):
         correct = x[:]
         seg_list = list(jieba.cut(x))
         rand = random.randint(0, len(seg_list)-1)
@@ -213,8 +223,8 @@ class PronounceSimilarVocabMaker(BaseDataMaker):
 
         try:
             similar_pronounce_spans = self.p2w.find_similar_vocab(span)
-        except:
-            raise FindOrConvertError
+        except Exception as exc:
+            raise FindOrConvertError from exc
         if len(similar_pronounce_spans) == 0:
             raise DataNotFundError('similar_pronounce_spans not found')
         random.shuffle(similar_pronounce_spans)
@@ -229,15 +239,16 @@ class PronounceSimilarVocabMaker(BaseDataMaker):
             incorrect=x
         )
 
+
 class PronounceSameVocabMaker(BaseDataMaker):
-    def __init__(self,*args,p2w=None,**kwargs):
+    def __init__(self, *args, p2w=None, **kwargs):
         super().__init__()
         if p2w is not None:
             self.p2w = p2w
         else:
             self.p2w = Pronounce2Word()
 
-    def make(self,x):
+    def make(self, x):
         correct = x[:]
         seg_list = list(jieba.cut(x))
         rand = random.randint(0, len(seg_list)-1)
@@ -245,8 +256,8 @@ class PronounceSameVocabMaker(BaseDataMaker):
 
         try:
             similar_pronounce_spans = self.p2w.find_same_vocab(span)
-        except:
-            raise FindOrConvertError
+        except Exception as exc:
+            raise FindOrConvertError from exc
 
         if len(similar_pronounce_spans) == 0:
             raise DataNotFundError('similar_pronounce_spans not found')
@@ -263,15 +274,16 @@ class PronounceSameVocabMaker(BaseDataMaker):
             incorrect=x
         )
 
+
 class RedundantWordMaker(BaseDataMaker):
-    def __init__(self,*args,p2w=None,**kwargs):
+    def __init__(self, *args, p2w=None, **kwargs):
         super().__init__()
         if p2w is not None:
             self.p2w = p2w
         else:
             self.p2w = Pronounce2Word()
-            
-    def make(self,x):
+
+    def make(self, x):
         correct = x[:]
         rand = random.randint(0, len(x)-1)
         x = list(x)
@@ -283,21 +295,23 @@ class RedundantWordMaker(BaseDataMaker):
             incorrect=x
         )
 
+
 class MistakWordMaker(BaseDataMaker):
-    def __init__(self,*args,p2w=None,**kwargs):
+    def __init__(self, *args, p2w=None, **kwargs):
         super().__init__()
         if p2w is not None:
             self.p2w = p2w
         else:
             self.p2w = Pronounce2Word()
-    def make(self,x):
+
+    def make(self, x):
         ch_unis = list(self.p2w.uni2cns_map.keys())
         random_ch_uni_index = random.randint(0, len(ch_unis))
 
         try:
             random_ch = self.p2w._uni2word(ch_unis[random_ch_uni_index])
-        except:
-            raise FindOrConvertError("p2w._uni2word out of range")
+        except Exception as exc:
+            raise FindOrConvertError("p2w._uni2word out of range") from exc
 
         correct = x[:]
         rand = random.randint(0, len(x)-1)
@@ -312,6 +326,7 @@ class MistakWordMaker(BaseDataMaker):
             incorrect=x
         )
 
+
 class MistakeWordHighFreqMaker(BaseDataMaker):
     def setup(self):
         self.high_freq_zh_char = []
@@ -320,7 +335,7 @@ class MistakeWordHighFreqMaker(BaseDataMaker):
             self.high_freq_zh_char.append(line.replace('\n', ''))
         f.close()
 
-    def make(self,x):
+    def make(self, x):
         random_ch_uni_index = random.randint(0, len(self.high_freq_zh_char)-1)
         random_ch = self.high_freq_zh_char[random_ch_uni_index]
 
@@ -337,6 +352,7 @@ class MistakeWordHighFreqMaker(BaseDataMaker):
             incorrect=x
         )
 
+
 class MissingWordHighFreqMaker(BaseDataMaker):
     def setup(self):
         self.high_freq_zh_char = []
@@ -345,7 +361,7 @@ class MissingWordHighFreqMaker(BaseDataMaker):
             self.high_freq_zh_char.append(line.replace('\n', ''))
         f.close()
 
-    def make(self,x):
+    def make(self, x):
         high_freq_char_list = []
         for char_x in list(x):
             if char_x in self.high_freq_zh_char:
@@ -368,21 +384,22 @@ class MissingWordHighFreqMaker(BaseDataMaker):
             incorrect=x
         )
 
+
 class RandomInsertVacabMaker(BaseDataMaker):
     def setup(self):
         sc_dict_path = os.path.join(
             os.path.dirname(py_chinese_pronounce.__file__),
             'sc-dict.txt'
         )
-        self.sc_dict = open(sc_dict_path,'r').read().split()
+        self.sc_dict = open(sc_dict_path, 'r', encoding='utf-8').read().split()
 
-    def make(self,x):
+    def make(self, x):
         correct = x[:]
         x = x[:]
-        random_vocab = self.sc_dict[random.randint(0,len(self.sc_dict)-1)]
-        rand_ins_postion = random.randint(0,len(x))
+        random_vocab = self.sc_dict[random.randint(0, len(self.sc_dict)-1)]
+        rand_ins_postion = random.randint(0, len(x))
         x = list(x)
-        x.insert(rand_ins_postion,random_vocab)
+        x.insert(rand_ins_postion, random_vocab)
         x = ''.join(x)
 
         return NoiseCorpus(
@@ -390,44 +407,47 @@ class RandomInsertVacabMaker(BaseDataMaker):
             incorrect=x
         )
 
+
 class Pipeline():
-    def __init__(self,makers=None,maker_weight=None):
+    def __init__(self, makers=None, maker_weight=None):
         self.maker_weight = maker_weight
         self.makers = makers
-        
+
         if makers is None:
             self.makers = []
             for g_var_name in copy(globals()):
                 try:
-                    if g_var_name not in [NoChangeMaker.__name__,BaseDataMaker.__name__] and issubclass(globals()[g_var_name],BaseDataMaker):
-                        print("O",g_var_name)
+                    if g_var_name not in [NoChangeMaker.__name__, BaseDataMaker.__name__] and issubclass(globals()[g_var_name], BaseDataMaker):
+                        print("O", g_var_name)
                         self.makers.append(globals()[g_var_name]())
                     else:
-                        print("X",g_var_name)
+                        print("X", g_var_name)
                 except:
-                    print("X",g_var_name)
+                    print("X", g_var_name)
 
         if self.maker_weight != None:
-            assert len(self.maker_weight) == len(self.makers),'While have `maker_weight` must provide maker_weight for each maker'
-    
-    def _noraml_call(self,x,k,no_change_on_gen_fail=False,verbose=True,makers=None):
+            assert len(self.maker_weight) == len(
+                self.makers), 'While have `maker_weight` must provide maker_weight for each maker'
+
+    def _noraml_call(self, x, k, no_change_on_gen_fail=False, verbose=True, makers=None):
         out = []
 
-        if makers == None:
+        if makers is None:
             makers = self.makers
 
         for maker in makers:
             retry = 0
-            while retry<5:
+            while retry < 5:
                 try:
                     res = maker(x)
-                    out.append(res)           
+                    out.append(res)
                     break
                 except Exception as e:
                     retry += 1
                     if verbose:
-                        logger.warning(f"{x} - {e} - {type(e)} - {maker} retry:{retry}")
-        
+                        logger.warning(
+                            f"{x} - {e} - {type(e)} - {maker} retry:{retry}")
+
         if len(out) == 0 and not no_change_on_gen_fail:
             raise DataGenerationError("Data gen fail")
         elif len(out) == 0 and no_change_on_gen_fail:
@@ -440,26 +460,25 @@ class Pipeline():
         random.shuffle(out)
         return out[:k]
 
-    def _weight_call(self,x,k, no_change_on_gen_fail, verbose=True):
+    def _weight_call(self, x, k, no_change_on_gen_fail, verbose=True):
         makers = random.choices(
             population=self.makers,
             weights=self.maker_weight,
             k=k
         )
 
-        return self._noraml_call(x,k,no_change_on_gen_fail,verbose,makers)
+        return self._noraml_call(x, k, no_change_on_gen_fail, verbose, makers)
 
-    def __call__(self,x,error_per_sent=1 ,no_change_on_gen_fail=False,verbose=True):
+    def __call__(self, x, error_per_sent=1, no_change_on_gen_fail=False, verbose=True):
         ori_x = x
-        assert error_per_sent>=1
+        assert error_per_sent >= 1
         for i in range(error_per_sent):
-            if self.maker_weight == None:
-                out = self._noraml_call(x,1,no_change_on_gen_fail,verbose)
+            if self.maker_weight is None:
+                out = self._noraml_call(x, 1, no_change_on_gen_fail, verbose)
                 x = out[0].incorrect
             else:
-                out = self._weight_call(x,1,no_change_on_gen_fail,verbose)
+                out = self._weight_call(x, 1, no_change_on_gen_fail, verbose)
                 x = out[0].incorrect
         for o in out:
             o.correct = ori_x
         return out[0]
-    
