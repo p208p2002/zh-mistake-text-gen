7f96c058ddfd456b237d023540364b0f95129d21
Philip <p208p2002@gmail.com>
2022-10-18 00:49:46 +0000

    style: pylint

--- zh_mistake_text_gen/data_model.py
+++ zh_mistake_text_gen/data_model.py
@@ -1,7 +1,10 @@
-from pydantic import BaseModel
 from typing import Optional
+from pydantic import BaseModel
 
 class NoiseCorpus(BaseModel):
+    """
+    output format
+    """
     type: Optional[str] = None
     correct: str
     incorrect: str
